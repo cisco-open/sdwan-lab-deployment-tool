32ed64ac708bb5be09af17431f347d135c28f8b6
larslilja <134637825+larslilja@users.noreply.github.com>
2024-09-05 12:39:20 +0200

    Create rc-PATty-example.sh

---
+++ rc-PATty-example.sh
@@ -0,0 +1,8 @@
+
+    export CML_IP='10.0.0.1'
+    export CML_USER='user1'
+    export CML_PASSWORD='2ajG$I2?8k'
+    export MANAGER_IP='pat:2002'
+    export MANAGER_USER='sdwan'
+    export MANAGER_PASSWORD='2ajG$I2?8k'
+    export LAB_NAME='sdwan-PATty'
